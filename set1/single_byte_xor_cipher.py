c6a9afd1540bd67a3b35a584e64910d75ab331a5
mpizzzle <michael770211@gmail.com>
2017-10-06 17:17:24 +0100

    updated set 1 challenge 3

--- set1/single_byte_xor_cipher.py
+++ set1/single_byte_xor_cipher.py
@@ -2,7 +2,6 @@ import sys
 
 hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
-for x in range(128):
-	plaintext = ''.join([chr(x ^ ord(a)) for a in hex.decode("hex")])
+for plaintext in [''.join([chr(x ^ ord(a)) for a in hex.decode("hex")]) for x in range(128)]:
 	if " a " in plaintext:
 		print plaintext

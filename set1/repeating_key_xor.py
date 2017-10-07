81ead5fb0d544b153aee2eca46d08603132db97e
mpizzzle <michael.770211@gmail.com>
2017-10-07 12:09:11 +0100

    set 1 challenge 5 complete

---
+++ set1/repeating_key_xor.py
@@ -0,0 +1,10 @@
+import sys
+
+def encrypt(key, msg):
+	return ''.join([chr(ord(key[i % len(key)]) ^ ord(char)) for i, char in enumerate(msg)])
+
+plaintext = "Burning 'em, if you ain't quick and nimble"
+plaintext2 = "I go crazy when I hear a cymbal"
+key = "ICE"
+
+print encrypt(key, plaintext + "\n" + plaintext2).encode("hex")

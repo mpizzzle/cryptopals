b04061a8d718ab0181f02ba9361ff37951baffdf
mpizzzle <michael.770211@gmail.com>
2017-10-07 11:26:48 +0100

    set 1 challenge 4 complete

---
+++ set1/detect_single_character_xor.py
@@ -0,0 +1,9 @@
+import sys
+
+with open('files/4.txt') as f:
+    hex_strings = f.read().splitlines()
+	
+for hex in hex_strings:
+	for plaintext in [''.join([chr(x ^ ord(a)) for a in hex.decode("hex")]) for x in range(128)]:
+		if " the " in plaintext:
+			print plaintext

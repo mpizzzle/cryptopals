ec1908d607e1536e792fdc710cca740a5cfbb21a
mpizzzle <michael.770211@gmail.com>
2017-11-05 21:39:36 +0000

    set 3 challenge 19/20 pretty much complete (I get the gist)

---
+++ set3/break_fixed_nonce_ctr_statistically.py
@@ -0,0 +1,42 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util import Counter
+
+frequent_letters = "etaoishr ETAOISHR"
+key = Random.new().read(AES.block_size)
+
+def encrypt(plaintext):
+    return AES.new(key, AES.MODE_CTR, counter=Counter.new(128)).encrypt(plaintext)
+
+def get_candidate_key_byte(transposed_block):
+    candidate = ''
+    candidate_frequency = 0
+
+    for c in range(256):
+        plaintext = ''.join([chr(c ^ ord(a)) for a in transposed_block])
+        frequency = sum([plaintext.count(frequent_letters[n]) for n in range(len(frequent_letters))])
+
+        if frequency > candidate_frequency:
+            candidate = chr(c)
+            candidate_frequency = frequency
+
+    return candidate
+
+def decrypt(key, msg):
+    return ''.join([chr(ord(key[i % len(key)]) ^ ord(char)) for i, char in enumerate(msg)])
+
+def get_key(file, key_length):
+    split_file = [file[i:i + key_length] for i in range(0, len(file), key_length)]
+    transposed_blocks = [''.join([block[x] for block in split_file[:len(split_file) - 1]]) for x in range(key_length)]
+    return ''.join([get_candidate_key_byte(block) for block in transposed_blocks])
+
+with open('files/20.txt') as f:
+    ciphertexts = [encrypt(line.decode("base64")) for line in f.read().splitlines()]
+
+key_length = min(len(ct) for ct in ciphertexts)
+ciphertext = ''.join([ct[:key_length] for ct in ciphertexts])
+found_key = get_key(ciphertext, key_length)
+plaintext = decrypt(found_key, ciphertext)
+
+for line in [plaintext[i:i + key_length] for i in range(0, len(plaintext), key_length)]:
+    print line
--- set3/break_fixed_nonce_ctr_using_substitutions.py
+++ set3/break_fixed_nonce_ctr_using_substitutions.py
@@ -4,8 +4,26 @@ from Crypto.Util import Counter
 
 key = Random.new().read(AES.block_size)
 
-with open('files/19.txt') as f:
-    ciphertexts = [AES.new(key, AES.MODE_CTR, counter=Counter.new(64,initial_value=0,little_endian=True,prefix="\x00\x00\x00\x00\x00\x00\x00\x00")).encrypt(line) for line in f.read().splitlines()]
+def encrypt(plaintext):
+    return AES.new(key, AES.MODE_CTR, counter=Counter.new(128)).encrypt(plaintext)
 
-for ct in ciphertexts:
-    print ct
+def xor(a, b):
+    return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b)])
+
+with open('files/19.txt') as f:
+    ciphertexts = [encrypt(line.decode("base64")) for line in f.read().splitlines()]
+
+def drag_crib(crib, a_xor_b):
+    derp = []
+    for n in range(len(a_xor_b) - len(crib) + 1):
+        potential = xor(crib, a_xor_b[n : n + len(crib)])
+
+        #if all(x.isalpha() or x.isspace() for x in potential):
+        #    if crib not in potential:
+        derp.append(potential)
+    print derp
+
+for i in range(40):
+    if i != 5:
+        print i
+        drag_crib("r polite ", xor(ciphertexts[i], ciphertexts[5]))
